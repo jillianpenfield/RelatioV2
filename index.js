0eedd6099f6bb184ee36700756c0f37615f7db33
Jillian Penfield <jpenfield@bestbeing.com>
2020-04-18 15:34:33 -0400

    Fixing customized Help

--- index.js
+++ index.js
@@ -133,6 +133,7 @@ function processMessage(event) {
       else if (analyzing) {
         analyzing = false;
         localMessages.push(formattedMsg);
+        console.log(localMessages);
         analyzeMessages(senderId, formattedMsg);
       } 
       else if(helping){
